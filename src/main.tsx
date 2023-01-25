82316b3d40c6cc570d55f761d125726dfeacd269
fikri amanda <fikriamd12@gmail.com>
2023-01-25 00:44:15 -1200

    feat: add page transaksi and pesanan saya

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Checkout from "./pages/Checkout";
+
 
 import App from "./routes";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Checkout />
+    <App />
   </React.StrictMode>
 );
--- src/pages/PesananSaya.tsx
+++ src/pages/PesananSaya.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Button from "../components/Button";
+import Layout from "../components/Layout";
 
 const PesananSaya = () => {
-  return <div>PesananSaya</div>;
+  return (
+    <Layout>
+      <div className="w-full h-full flex flex-col items-center bg-[#DCDCDC]">
+        <div className="flex-none gap-4 min-w-[70%] mt-11">
+          <div className="card w-full h-[8vh] bg-base-100 shadow-lg">
+            <div className="card-body flex-row justify-between">
+              <div>
+              <p className="text-lg hover:text-[#03AC0E]">Semua</p>
+              </div>
+              <div>
+              <p className="text-lg hover:text-[#03AC0E]">Belum bayar</p>
+              </div>
+              <div>
+              <p className="text-lg hover:text-[#03AC0E]">Sedang dikemas</p>
+              </div>
+              <div>
+              <p className="text-lg hover:text-[#03AC0E]">Dikirim</p>
+              </div>
+              <div>
+              <p className="text-lg hover:text-[#03AC0E]">Selesai</p>
+              </div>
+              <div>
+              <p className="text-lg hover:text-[#03AC0E]">Dibatalkan</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default PesananSaya;

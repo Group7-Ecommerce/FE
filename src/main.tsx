ca96750287d96ba43cc11abe92de93cf325a8924
Devi Andri Aldi <deviandrialdi@gmail.com>
2023-01-25 20:07:25 +0700

    Feat: Halaman Transaksi

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import "./styles/index.css";
 import DetailProduct from "./pages/DetailProduct";
 import Cardpost from "./components/CardPost";
 import ProfilUser from "./pages/ProfilUser";
-import Homepage from "./pages/Homepage";
+import HalamanTransaksi from "./pages/HalamanTransaksi";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Homepage />
+    <HalamanTransaksi />
   </React.StrictMode>
 );

e1966e5ba20486d1fb18e23e1dccdb2c3570dac0
Devi Andri Aldi <deviandrialdi@gmail.com>
2023-01-25 14:21:56 +0700

    Feat: Slicing HomePage and Beranda

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 interface LayoutProps {
@@ -10,9 +10,10 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="w-full h-screen flex flex-col overflow-auto">
       <Navbar />
-      <div className="h-full w-full bg-base-300 bg-center bg-cover bg-no-repeat flex flex-col">
+      <div className="h-full w-full bg-base-200 bg-[url('../assets/background.jpg')] bg-center bg-cover bg-no-repeat flex flex-col overflow-auto">
         {children}
       </div>
+      <Footer />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Register from "./pages/Auth/Register";
-import Footer from "./components/Footer";
+import App from "./routes";
 import "./styles/index.css";
 import DetailProduct from "./pages/DetailProduct";
 import Cardpost from "./components/CardPost";
 import ProfilUser from "./pages/ProfilUser";
+import Homepage from "./pages/Homepage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <footer />
+    <Homepage />
   </React.StrictMode>
 );

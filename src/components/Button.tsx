5e6d34d720da578af988d5382ab039efb659ac74
fikri amanda <fikriamd12@gmail.com>
2023-01-24 14:50:59 -1200

    feat: add components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { ButtonHTMLAttributes } from "react";
 
-const Button = () => {
-  return <div>Button</div>;
-};
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string;
+  loading?: boolean;
+}
+
+function Button({ id, label, loading, ...props }: ButtonProps) {
+  return (
+    <button
+      id={id}
+      className={`bg-green-500 text-white font-bold py-2 px-4 rounded-full border border-green-500 ${
+        loading && "cursor-not-allowed"
+      }`}
+      disabled={loading}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+}
 
 export default Button;
--- src/components/Cardakun.tsx
+++ src/components/Cardakun.tsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 const Cardakun = () => {
-  return <div>Cardakun</div>;
+  return (
+    <div className="card w-full bg-center justify-between">
+      <div className="card-body">
+        <h2 className="card-title">Edit Account</h2>
+        <div className="flex flex-col mt-8">
+       <label>
+        <span className="font-extrabold">Name </span>
+        <div className=" items-center text-center -mt-8">
+       <input 
+       type="text"
+       placeholder="enter your name..."
+       className="input input-bordered w-full max-w-xs border-black"/>
+       </div>
+       </label>
+      </div>
+        <div className="flex flex-col mt-8">
+       <label>
+        <span className="font-extrabold">Number Phone </span>
+        <div className=" items-center text-center -mt-7">
+       <input 
+       type="text"
+       placeholder="enter your number phone..."
+       className="input input-bordered w-full max-w-xs border-black"/>
+       </div>
+       </label>
+      </div>
+        <div className="flex flex-col mt-8">
+       <label>
+        <span className="font-extrabold">address </span>
+        <div className=" items-center text-center -mt-7">
+       <input 
+       type="text"
+       placeholder="enter your address..."
+       className="input input-bordered w-full max-w-xs border-black"/>
+       </div>
+       </label>
+      </div>
+      <div className="card-actions justify-end">
+      <button className="btn bg-gray">Update</button>
+    </div>
+    </div>
+    </div>
+  );
 };
 
 export default Cardakun;

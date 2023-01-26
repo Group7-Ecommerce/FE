23c92847b4943306afc851835c9fd3153517ce43
fikri amanda <fikriamd12@gmail.com>
2023-01-25 22:49:56 -1200

    feat: add router and set integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { PersistGate } from "redux-persist/integration/react";
 import ReactDOM from "react-dom/client";
-import App from "./routes";
+import { Provider } from "react-redux";
+import { store, persistor } from "./utils/redux/store/store";
+
+import Router from "./routes/Router";
 import "./styles/index.css";
 
+import Navbar from "./components/Navbar";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router />
+    </PersistGate>
+  </Provider>
 );

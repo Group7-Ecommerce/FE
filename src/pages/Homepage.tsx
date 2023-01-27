1f3418350467a32582b48aa2d9b77f34a409e4de
fikri amanda <fikriamd12@gmail.com>
2023-01-27 11:29:06 -1200

    feat: add example cart

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,7 @@
 import withReactContent from "sweetalert2-react-content";
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -16,44 +17,51 @@ import Button from "../components/Button";
 import Card from "../components/Card";
 import { setCart } from "../utils/redux/reducers/reducer";
 
+
+
 const Homepage = () => {
-  const { id } = useParams()
+  const { id } = useParams();
   const dispatch = useDispatch();
   const [datas, setDatas] = useState<HomepageType[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [cookie, setCookie] = useCookies();
+  const [totalPrice, setTotalPrice] = useState<number>();
+  const [totalQty, SetTotalQty] = useState<number>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchData;
   }, []);
 
   function fetchData() {
-    axios.get(`products/${id}`)
-    .then((res) => {
-      const { data } = res.data.data;
-      setDatas(data);
-    })
-    .catch((err) => {
-      alert(err.toString());
-    })
-    .finally(() => setLoading(false));
+    axios
+      .get("http://34.69.18.136/products", {
+        headers: {
+          Authorization: `Bearer ${cookie.token}`,
+        },
+      })
+      .then((res) => {
+        const { data } = res.data;
+
+        setCart(data);
+        const totalQty = data.reduce((a: any, b: any) => {
+          return a + b.qty;
+        }, 0);
+        const totalPrc = data.reduce((a: any, b: any) => {
+          return a + b.total_price;
+        }, 0);
+        SetTotalQty(totalQty);
+        setTotalPrice(totalPrc);
+      })
+
+      .catch((err) => {});
   }
 
-  function handleCart(data: HomepageType) {
-    const checkExist = localStorage.getItem("Cart");
-    if (checkExist) {
-      let parseCart: HomepageType[] = JSON.parse(checkExist);
-      parseCart.push(data);
-      localStorage.setItem("Cart", JSON.stringify(parseCart));
-      dispatch(setCart(parseCart));
-    } else {
-      localStorage.setItem("Cart", JSON.stringify([data]));
-      alert("Items add to Cart");
-    }
-  }
+  
 
   return (
     <Layout>
-      <div className="m-2 grid grid-flow-row auto-rows-max grid-cols-2 gap-3 md:grid-cols-4 lg:grid-cols-5">
+      <div className="m-2 grid grid-flow-row auto-rows-max grid-cols-2 gap-3 md:grid-cols-4 lg:grid-cols-5 bg-black">
         {loading
           ? [...Array(5).keys()].map((data) => <SkeletonLoading key={data} />)
           : datas.map((data) => (
@@ -63,12 +71,12 @@ const Homepage = () => {
                 image={data.image}
                 id={data.id}
                 labelButton="ADD TO CART"
-                onClickCart={() => handleCart(data)}
+                // onClickCart={() => handleCart(data)}
               />
             ))}
       </div>
     </Layout>
-  )
+  );
 };
 
 export default Homepage;
